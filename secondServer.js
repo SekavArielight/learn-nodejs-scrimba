8347a56e1870defde89f17da00c3acfa03c5e480
God'swill Sekav Apya <godswillapya@gmail.com>
2025-06-10 22:24:45 +0100

    Refactor code formatting for consistency in secondServer.js

--- secondServer.js
+++ secondServer.js
@@ -1,31 +1,38 @@
-import http from "node:http";
-import { getDataFromDB } from "./database/db.js";
+import http from 'node:http'
+import { getDataFromDB } from './database/db.js'
 
-const PORT = 8000;
+const PORT = 8000
 
 const server = http.createServer(async (req, res) => {
-  const destinations = await getDataFromDB();
+  const destinations = await getDataFromDB()
 
-  if (req.url === "/api" && req.method === "GET") {
-    res.setHeader("Content-Type", "application/json");
-    res.statusCode = 200;
-    res.end(JSON.stringify(destinations));
+  if (req.url === '/api' && req.method === 'GET') {
+
+    res.setHeader('Content-Type', 'application/json')
+    res.statusCode = 200
+    res.end(JSON.stringify(destinations))
+
+  } else if (req.url.startsWith('/api/continent') && req.method === 'GET') {
+
+    const continent = req.url.split('/').pop()
+    const filteredData = destinations.filter((destination) => {
+      return destination.continent.toLowerCase() === continent.toLowerCase()
+    })
+    res.setHeader('Content-Type', 'application/json')
+    res.statusCode = 200
+    res.end(JSON.stringify(filteredData))
+
   } else {
-    /*
-Challenge:
-  1. If the client tries to access a route that isn’t covered by the above, send this object: 
-      {error: "not found", message: "The requested route does not exist"}
-  Think: what do we need to send along with the data?
-*/
-    res.setHeader("Content-Type", "application/json");
-    res.statusCode = 404;
-    res.end(
-      JSON.stringify({
-        error: "not found",
-        message: "The requested route does not exist",
-      })
-    );
-  }
-});
 
-server.listen(PORT, () => console.log(`Connected on port: ${PORT}`));
+    res.setHeader('Content-Type', 'application/json')
+    res.statusCode = 404
+    res.end(JSON.stringify({
+      error: "not found",
+      message: "The requested route does not exist"
+    })
+    )
+  }
+  
+})
+
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
